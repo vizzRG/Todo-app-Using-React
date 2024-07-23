6df9a5352608690f7853f5b659919d823d600a98
vizzRG <vizzrg@gmail.com>
2024-07-23 14:43:56 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
   return (
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
     <div className="bg-[#172842] min-h-screen py-8">
-                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white border  border-blue-400 border-[2px]">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white border-2 border-blue-400">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
