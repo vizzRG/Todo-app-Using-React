24cb6972b3b1cd6ed0800658daf792f37a879311
vizzRG <vizzrg@gmail.com>
2024-07-23 14:06:15 +0530

    Created Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 import { TodoProvider } from './context'
@@ -18,10 +18,22 @@ function App() {
     setTodos((prev)=>(prev.filter((prevTodo)=>(prevTodo.id !== id))))
   }
 
-  const toggleComplete = (id)=>{
+  const toggleCompleted = (id)=>{
     setTodos((prev)=>(prev.map((prevTodo)=>(prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed } : prevTodo))))
   }
 
+
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length > 0){
+      setTodos(todos)
+    }
+  },[])
+
+  useEffect(()=> {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  },[todos])
 
   return (
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import TodoForm from "./TodoForm";
+import TodoItem from "./TodoItems";
+
+
+export {TodoForm, TodoItem}
